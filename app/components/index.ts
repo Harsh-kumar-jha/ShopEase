7475a4f047945768c7a0fbd9fa019af2daf3a532
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2023-10-20 14:06:57 +0530

    Created a Product page and display a dummy products on the home page

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,5 +2,6 @@ import Footer from "./footer/Footer";
 import NavBar from "./navbar/NavBar";
 import HomeBanner from "./HomeBanner/HomeBanner";
 import Container from "./Container";
+import ProductCart from "./products/ProductCart";
 
-export { Footer, NavBar, HomeBanner, Container };
+export { Footer, NavBar, HomeBanner, Container, ProductCart };
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import { Container, HomeBanner } from "./components";
+import { products } from "@/utils/products";
+import { Container, HomeBanner, ProductCart } from "./components";
+import { truncateName } from "@/utils/truncateName";
 
 export default function Home() {
   return (
@@ -6,6 +8,15 @@ export default function Home() {
       <Container>
         <div>
           <HomeBanner />
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols lg:grid-cols-4 xl-grid-cols-5 2xl:grid-cols-6 gap-8">
+          {products.map((product: any) => {
+            return (
+              <div key={product.id}>
+                <ProductCart data={product} />
+              </div>
+            );
+          })}
         </div>
       </Container>
     </div>
